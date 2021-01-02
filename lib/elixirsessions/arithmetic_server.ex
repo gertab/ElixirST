77ad09318ec0854a859117638f863666b140d82a
Gerard <gerardtabone@yahoo.com>
2021-01-02 15:12:30 +0100

    simple arithmetic server/client

---
+++ lib/elixirsessions/arithmetic_server.ex
@@ -0,0 +1,50 @@
+defmodule ElixirSessions.ArithmeticServer do
+  def run() do
+    server = spawn(__MODULE__, :arith_serv, [])
+    attempt1(server)
+
+    server = spawn(__MODULE__, :arith_serv, [])
+    attempt2(server)
+  end
+
+  # @session "branch(add: receive {number, number, pid} . send {number}, neg: receive {number, pid} . send {number})"
+  def arith_serv() do
+    receive do
+      {:add} ->
+        receive do
+          {num1, num2, pid} ->
+            IO.puts("[server] #{num1} + #{num2}")
+            send(pid, {num1 + num2})
+        end
+
+      {:neg} ->
+        receive do
+          {num, pid} ->
+            IO.puts("[server] neg of #{num}")
+            send(pid, {-num})
+        end
+    end
+  end
+
+  # @session "choice(add: send {number, number, pid} . receive {number})"
+  def attempt1(server) when is_pid(server) do
+    send(server, {:add})
+    send(server, {34, 54, self()})
+
+    receive do
+      {value} ->
+        IO.puts("[client] = #{value}")
+    end
+  end
+
+  # @session "choice(neg: send {number, pid} . receive {number})"
+  def attempt2(server) when is_pid(server) do
+    send(server, {:neg})
+    send(server, {54, self()})
+
+    receive do
+      {value} ->
+        IO.puts("[client] = #{value}")
+    end
+  end
+end
