4239ed7d1ca7f8989b29e346501841e4db266c6d
Gerard <gerardtabone@yahoo.com>
2021-01-02 10:20:55 +0100

    initial commit

---
+++ lib/elixirsessions.ex
@@ -0,0 +1,18 @@
+defmodule Elixirsessions do
+  @moduledoc """
+  Documentation for Elixirsessions.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirsessions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirsessions_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirsessionsTest do
+  use ExUnit.Case
+  doctest Elixirsessions
+
+  test "greets the world" do
+    assert Elixirsessions.hello() == :world
+  end
+end
