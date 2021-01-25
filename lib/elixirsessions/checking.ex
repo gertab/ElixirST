b707ec870c401539a086d8c9f0c7c572716e1290
Gerard <gerardtabone@yahoo.com>
2021-01-25 13:38:01 +0100

    Cleaned module Checking

--- lib/elixirsessions/checking.ex
+++ lib/elixirsessions/checking.ex
@@ -7,37 +7,21 @@ defmodule ElixirSessions.Checking do
 
   defmacro __using__(_) do
     quote do
-      # import Kernel, except: [@: 1]
       import ElixirSessions.Checking
 
       Module.register_attribute(__MODULE__, :session, accumulate: true)
 
-      # @before_compile ElixirSessions.Checking
       @on_definition ElixirSessions.Checking
-
-      # @session %{}
-      # @session nil
-      # import ElixirSessions.Checking
     end
   end
 
-  # defmacro __before_compile__(_env) do
-    # definitions = Module.definitions_in(env.module)
-    # contracts = Module.get_attribute(env.module, :session)
+  # Definition of a function head, therefore do nothing
+  def __on_definition__(_env, _access, _name, _args, _guards, nil), do: nil
+  def __on_definition__(_env, _access, _name, _args, _guards, []), do: nil
 
-    # IO.inspect(definitions)
-    # IO.inspect(contracts)
-
-    # IO.inspect(__MODULE__.__info__)
-  # end
-
-  def __on_definition__(env, _kind, fun, _args, _guards, body) do
+  def __on_definition__(env, _access, _name, _args, _guards, _body) do
 
     if sessions = Module.get_attribute(env.module, :session) do
-      # sessions = Module.get_attribute(env.module, :session)
-      # sessions = Map.put(sessions, {kind, fun, length(args)}, session)
-      # Module.put_attribute(env.module, :session, sessions)
-      # Module.put_attribute(env.module, :session, nil)
 
       if length(sessions) > 0 do
         [session | _ ] = sessions
@@ -45,43 +29,14 @@ defmodule ElixirSessions.Checking do
         s = ElixirSessions.Parser.parse(session)
         case s do
           {:ok , _session_type} -> :ok
-          # {:ok , session_type} -> ElixirSessions.Code.walk_ast(fun, body, session_type)
+          # {:ok , session_type} -> ElixirSessions.Code.walk_ast(name, body, session_type)
           _                    -> Logger.error("Leex error")
         end
         # ElixirSessions.Parser.parse(session)
-        # IO.inspect(fun)
+        # IO.inspect(name)
       end
     end
     # IO.inspect(body)
     :ok
   end
-
-  # defmacro __before_compile__(env) do
-  #   decorators = Module.get_attribute(env.module, :session)
-  #   for {{kind, fun, arity}, decorator} <- decorators do
-  #     args = generate_args(arity)
-  #     body = decorator.decorate(kind, fun, args)
-  #     quote do
-  #       defoverridable [{unquote(fun), unquote(arity)}]
-
-  #       Kernel.unquote(kind)(unquote(fun)(unquote_splicing(args))) do
-  #         unquote(body)
-  #       end
-  #     end
-  #   end
-  # end
-  # defp generate_args(0), do: []
-  # defp generate_args(n), do: for(i <- 1..n, do: Macro.var(:"var#{i}", __MODULE__))
-
-
-  # defmacro @{:session, _, expr} do
-  #   # defcontract(expr, __CALLER__.line)
-  #   IO.puts(expr)
-  # end
-
-  # defmacro @other do
-  #   quote do
-  #     Kernel.@(unquote(other))
-  #   end
-  # end
 end
