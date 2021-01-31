dfc5ef2acaace6192364537d3771b5c60af5f326
Gerard <gerardtabone@yahoo.com>
2021-01-31 14:49:47 +0100

    Updated github workflow

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ElixirSessions.MixProject do
       app: :elixirsessions,
       version: "0.1.0",
       elixir: "~> 1.9",
+      dialyzer: dialyzer(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,6 +26,13 @@ defmodule ElixirSessions.MixProject do
       {:dialyzex, "~> 1.2.0", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 end
