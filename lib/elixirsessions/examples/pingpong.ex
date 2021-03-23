bdb1c80c3eefd714427f6f8333f6fe6527d9357f
Gerard <gerardtabone@yahoo.com>
2021-03-23 16:03:11 +0100

    Commenting the examples

--- lib/elixirsessions/examples/pingpong.ex
+++ lib/elixirsessions/examples/pingpong.ex
@@ -1,107 +1,127 @@
-defmodule ElixirSessions.PingPong do
-  use ElixirSessions.Checking
+# defmodule ElixirSessions.PingPong do
+#   use ElixirSessions.Checking
 
-  def run() do
-    IO.puts("Spawning process")
-    ponger = spawn(__MODULE__, :pong, [])
-    IO.puts("Process spawned as #{inspect(ponger)}")
+#   def run() do
+#     IO.puts("Spawning process")
+#     ponger = spawn(__MODULE__, :pong, [])
+#     IO.puts("Process spawned as #{inspect(ponger)}")
 
-    ping(ponger)
-    pong(ponger)
-  end
+#     ping(ponger)
+#     pong(ponger)
+#   end
 
-  @session "ping = !ping(any).?pong().end"
-  def ping(pid) when is_pid(pid) do
-    IO.puts("Sending ping to #{inspect(pid)}")
+#   @session "ping = !ping(any).?pong().end"
+#   def ping(pid) when is_pid(pid) do
+#     IO.puts("Sending ping to #{inspect(pid)}")
 
-    send(pid, {:ping, self()})
+#     send(pid, {:ping, self()})
 
-    receive do
-      {:pong} ->
-        IO.puts("Received pong!")
-    end
-  end
+#     receive do
+#       {:pong} ->
+#         IO.puts("Received pong!")
+#     end
+#   end
 
-  # @session "end"
-  # defp ppp() do
-  #   if 2 + 3 < 2 do
-  #     :ok
-  #   end
+#   # @session "end"
+#   # defp ppp() do
+#   #   if 2 + 3 < 2 do
+#   #     :ok
+#   #   end
 
-  #   with a <- :ok do
-  #     a
-  #   end
+#   #   with a <- :ok do
+#   #     a
+#   #   end
 
-  #   a = 3
+#   #   a = 3
 
-  #   cond do
-  #     3 + 2 < 2 ->
-  #       :ok
-  #     a ->
-  #       :okk
-  #   end
-  # end
+#   #   cond do
+#   #     3 + 2 < 2 ->
+#   #       :ok
+#   #     a ->
+#   #       :okk
+#   #   end
+#   # end
 
-  # @session "pong/0 = ?ping(any).!pong()"
-  def pong() do
-    receive do
-      {:ping, pid} ->
-        IO.puts(
-          "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
-            "to #{inspect(pid)}"
-        )
+#   # @session "pong/0 = ?ping(any).!pong()"
+#   def pong() do
+#     receive do
+#       {:ping, pid} ->
+#         IO.puts(
+#           "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
+#             "to #{inspect(pid)}"
+#         )
 
-        send(pid, {:pong})
-    end
-  end
+#         send(pid, {:pong})
+#     end
+#   end
 
-  @session "S_1 = !helloooo().!helloooo2().ping"
-  @session "pong/2 = ?ping(any).!pong().S_1"
-  def pong(hello) do
+#   def pong(hello) do
+#   end
 
-  end
-  def pong(hello, _hello2, :okk) when is_atom(hello) do
-      b = hello + 2
-    _a = b
+#   @session "pong/2 = ?ping(any).!pong().!helloooo()"
+#   def pong(hello, _hello2) when is_atom(hello) do
+#     b = hello + 2
 
-    receive do
-      {:ping, pid} ->
-        IO.puts(
-          "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
-            "to #{inspect(pid)}"
-        )
+#     case true do
+#       false ->
+#         receive do
+#           {:ping, pid} ->
+#             IO.puts(
+#               "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
+#                 "to #{inspect(pid)}"
+#             )
 
-        send(pid, {:pong})
-    end
+#             send(pid, {:pong})
+#         end
 
-    so_something()
+#       _ ->
+#         receive do
+#           {:ping, pid} ->
+#             IO.puts(
+#               "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
+#                 "to #{inspect(pid)}"
+#             )
 
-    ping(self())
-  end
+#             send(pid, {:pong})
+#         end
+#     end
 
-  def so_something() do
-    send(self(), {:helloooo})
-    send(self(), {:helloooo2})
+#     case true do
+#       false ->
+#         send(self(), {:helloooo})
 
-    # abc()
-  end
+#       _ ->
+#         send(self(), {:helloooo})
+#     end
 
-  @session "loop = rec X.(!hello().X)"
-  def loop() do
-    send(self(), {:hello})
+#     # so_something()
 
-    loop()
-  end
+#     # ping(self())
+#   end
 
-  @session "abc = rec X.(&{?hello().!aaa().X, ?stop()})"
-  def abc() do
-    receive do
-      {:hello} ->
-        send(self(), {:aaa})
-        abc()
+#   def so_something() do
+#     send(self(), {:helloooo})
+#     send(self(), {:helloooo2})
 
-      {:stop} ->
-        :ok
-    end
-  end
-end
+#     # abc()
+#   end
+
+#   # @session "loop = rec X.(!hello().X)"
+#   # def loop() do
+#   #   send(self(), {:hello})
+
+#   #   loop()
+#   # end
+
+#   # @session "abc = rec X.(&{?hello().!aaa().X, ?stop()})"
+#   # def abc() do
+#   #   receive do
+#   #     {:hello} ->
+#   #       send(self(), {:aaa})
+#   #       abc()
+
+#   #     {:stop} ->
+#   #       :ok
+#   #   end
+#   # end
+# end
--- lib/elixirsessions/examples/simple_example.ex
+++ lib/elixirsessions/examples/simple_example.ex
@@ -1,73 +1,161 @@
-defmodule ElixirSessions.SmallExample do
-  use ElixirSessions.Checking
-  @moduledoc false
-  # iex -S mix
+# defmodule ElixirSessions.SmallExample do
+#   use ElixirSessions.Checking
+#   @moduledoc false
+#   # iex -S mix
 
-  def run() do
-    spawn(__MODULE__, :example1, [])
-  end
-
-
-
-
-
-  @session "example1 = rec X.(!ok().?something(any).X)"
-  def example1() do
-    send(self(), {:ok})
-
-    receive do
-      {:something, _} ->
-        :ok
-    end
-
-
-    example1()
-  end
+#   # def run() do
+#   #   spawn(__MODULE__, :example1, [])
+#   # end
 
 
 
 
 
-  @session "example2 = !ok().?something(any)"
-  def example2() do
-    send_call()
-
-    receive do
-      {:something, _} ->
-        :ok
-    end
-  end
-
-  @session "send_call = !ok()"
-  def send_call() do
-    send(self(), {:ok})
-  end
 
 
 
+#   @session "function1 = rec X.(   !ok().rec Y.(   &{?option1().X, ?option2().Y}   )   )"
+#   def function1(pid) do
+#     send(pid, {:ok})
 
-  @session "S_1 = !ok3().!ok4()"
-  @session "example3 = !ok1().!ok2().S_1"
-  def example3() do
-    pid = self()
+#     function2(pid)
+#   end
 
-    send(pid, {:ok1})
-    send(pid, {:ok2})
-    send(pid, {:ok3})
-    send(pid, {:ok4})
-  end
+#   def function2(pid) do
+#     receive do
+#       {:option1} -> function1(pid)
+#       {:option2} -> function2(pid)
+#     end
+#   end
 
 
 
 
-  @session "problem = ?label(any).!num(any)"
-  def problem() do
-    # a = 5
-    receive do
-      {:label, _value} ->
-        :ok
-    end
 
-    send(self(), {:num, 55})
-  end
-end
+
+#   # send in diff function
+#   @session "function1 = rec X.(   !ok().rec Y.(   &{?option1().X, ?option2().Y}   )   )"
+#   def function1(pid) do
+#     function_send(pid)
+#     function2(pid)
+#   end
+
+#   def function_send(pid) do
+#     send(pid, {:ok})
+#   end
+
+#   def function2(pid) do
+#     receive do
+#       {:option1} -> function1(pid)
+#       {:option2} -> function2(pid)
+#     end
+#   end
+
+
+
+
+
+
+#   @session "function1 = rec X.(   !ok().rec Y.(   &{?option1().X, ?option2().Y, ?option3().!finish()}   )   )"
+#   def function1(pid) do
+#     send(pid, {:ok})
+
+#     function2(pid)
+
+#     send(pid, {:finish})
+#   end
+
+#   def function2(pid) do
+#     receive do
+#       {:option1} -> function1(pid)
+#       {:option2} -> function2(pid)
+#       {:finish} -> nil
+#     end
+#   end
+
+
+#   # @session "dooo = rec X.(&{?A().rec Z.(!C().Z),   ?B().rec Y.(+{!D(), !E().Y, !F().X}),   ?C() })"
+#   # def dooo() do
+#   #   pid = self()
+#   #   receive do
+#   #     {:A} ->
+#   #       firstRec(pid)
+#   #     {:B} ->
+#   #       secondRec(pid)
+#   #     {:C} -> :ok
+#   #   end
+
+#   # end
+
+#   # # @session "firstRec = rec Y.(!C().Y)"
+#   # def firstRec(pid) do
+#   #   send(pid, {:C})
+
+#   #   firstRec(pid)
+#   # end
+
+#   # @session "secondRec = rec Y.(+{!D(), !E().Y, !F()})"
+#   # def secondRec(pid) do
+#   #   case true do
+#   #     false ->
+#   #       send(pid, {:D})
+
+#   #     2 ->
+#   #       send(pid, {:E})
+#   #       secondRec(pid)
+
+#   #     true ->
+#   #       send(pid, {:F})
+#   #       dooo()
+#   #   end
+#   # end
+
+#   @session "example1 = rec X.(!okkkkk().?something(any).X)"
+#   def example1() do
+#     send(self(), {:okkkkk})
+
+#     receive do
+#       {:something, _} ->
+#         :ok
+#     end
+
+#     example1()
+#   end
+
+#   @session "example2 = !ok().?something(any)"
+#   def example2() do
+#     send_call()
+
+#     receive do
+#       {:something, _} ->
+#         :ok
+#     end
+#   end
+
+#   @session "send_call = !ok()"
+#   def send_call() do
+#     send(self(), {:ok})
+#   end
+
+#   @session "S_1 = !ok3().!ok4()"
+#   @session "example3 = !ok1().!ok2().S_1"
+#   def example3() do
+#     pid = self()
+
+#     send(pid, {:ok1})
+#     send(pid, {:ok2})
+#     send(pid, {:ok3})
+#     send(pid, {:ok4})
+#   end
+
+#   # @session "problem = ?label(any).!num(any)"
+#   # def problem() do
+#   #   # a = 5
+#   #   receive do
+#   #     {:label, _value} ->
+#   #       :ok
+#   #   end
+
+#   #   send(self(), {:num, 55})
+#   # end
+# end
