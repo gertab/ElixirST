5d78bef90963cf3ef0a4fbdf9e107bd75d9c492c
Gerard <gerardtabone@yahoo.com>
2021-04-19 16:04:40 +0200

    'Fixed examples

--- lib/elixirsessions/examples/pingpong.ex
+++ lib/elixirsessions/examples/pingpong.ex
@@ -1,71 +1,15 @@
 defmodule ElixirSessions.PingPong do
   use ElixirSessions.Checking
 
-  def main() do
-    bbb(self())
+  def run() do
+    IO.puts("Spawning process")
+    pinger = spawn(__MODULE__, :ping, [])
+    _ponger = spawn(__MODULE__, :pong, [pinger])
+    IO.puts("Process spawned as #{inspect(pinger)}")
   end
 
-  # def run() do
-  #   IO.puts("Spawning process")
-  #   ponger = spawn(__MODULE__, :pong, [])
-  #   IO.puts("Process spawned as #{inspect(ponger)}")
-
-  #   ping(ponger)
-  #   pong(ponger)
-  # end
-
-  # @session "ping = !ping(any).?pong()"
-  @session "!ping(any).rec X.(?pong().X)"
-  def ping(pid) when is_pid(pid) do
-    IO.puts("Sending ping to #{inspect(pid)}")
-
-    send(pid, {:ping, self()})
-
-    kk()
-  end
-
-  def kk() do
-    receive do
-      {:pong} ->
-        IO.puts("Received pong!")
-    end
-
-    kk()
-  end
-
-  @session "rec X.(&{?A().X, ?B().!C()})"
-  def hello() do
-    receive do
-      {:A} -> hello()
-      {:B} -> nil
-      send(self(), {:C})
-    end
-  end
-
-  # @session "end"
-  # defp ppp() do
-  #   if 2 + 3 < 2 do
-  #     :ok
-  #   end
-
-  #   with a <- :ok do
-  #     a
-  #   end
-
-  #   a = 3
-
-  #   cond do
-  #     3 + 2 < 2 ->
-  #       :ok
-  #     a ->
-  #       :okk
-  #   end
-  # end
-
-
-
-  @session "?ping(number).!pong()"
-  def pong() do
+  @session "rec X.(?ping(pid).!pong().X)"
+  def ping() do
     receive do
       {:ping, pid} ->
         IO.puts(
@@ -75,106 +19,19 @@ defmodule ElixirSessions.PingPong do
 
         send(pid, {:pong})
     end
+
+    ping()
   end
 
+  @dual &ElixirSessions.PingPong.ping/0
+  def pong(pid) do
+    send(pid, {:ping, self()})
 
-
-
-  # def pong(hello) do
-  # end
-
-  # @session "pong/2 = ?ping(any).!pong().!helloooo()"
-  # def pong(hello, _hello2) when is_atom(hello) do
-  #   b = hello + 2
-
-  #   case true do
-  #     false ->
-  #       receive do
-  #         {:ping, pid} ->
-  #           IO.puts(
-  #             "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
-  #               "to #{inspect(pid)}"
-  #           )
-
-  #           send(pid, {:pong})
-  #       end
-
-  #     _ ->
-  #       receive do
-  #         {:ping, pid} ->
-  #           IO.puts(
-  #             "Received ping from #{inspect(pid)}. Replying pong from #{inspect(self())} " <>
-  #               "to #{inspect(pid)}"
-  #           )
-
-  #           send(pid, {:pong})
-  #       end
-  #   end
-
-  #   case true do
-  #     false ->
-  #       send(self(), {:helloooo})
-
-  #     _ ->
-  #       send(self(), {:helloooo})
-  #   end
-
-  #   # so_something()
-
-  #   # ping(self())
-  # end
-
-  @session "!helloooo().!helloooo2()"
-  def do_something(5555) do
-    send(self(), {:helloooo})
-    send(self(), {:helloooo2})
-
-    # abc()
-  end
-
-  # def do_something(number) do
-  #   send(self(), {:helloooo})
-  #   send(self(), {:helloooo2})
-
-  #   # abc()
-  # end
-
-  # @session "loop = rec X.(!hello().X)"
-  # def loop() do
-  #   send(self(), {:hello})
-
-  #   loop()
-  # end
-
-  # @session "abc = rec X.(&{?hello().!aaa().X, ?stop()})"
-  # def abc() do
-  #   receive do
-  #     {:hello} ->
-  #       send(self(), {:aaa})
-  #       abc()
-
-  #     {:stop} ->
-  #       :ok
-  #   end
-  # end
-
-  @session "!hello().!hello().!hello2()"
-  def aaa(pid) do
-    send(pid, {:hello})
-    send(pid, {:hello})
-    send(pid, {:hello2})
-  end
-
-  @dual &ElixirSessions.PingPong.aaa/1
-  def bbb(_pid) do
     receive do
-      {:hello} -> :ok
+      {:pong} ->
+        IO.puts("Received pong.")
     end
-    receive do
-      {:hello} -> :ok
-    end
-    receive do
-      {:hello2} -> :ok
-    end
+
+    pong(pid)
   end
 end
