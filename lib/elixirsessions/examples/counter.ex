a192e57a8a47c61e03aa466963befd4a20fea30d
Gerard <gerardtabone@yahoo.com>
2021-07-28 10:55:55 +0200

    Counter system example

--- lib/elixirsessions/examples/counter.ex
+++ lib/elixirsessions/examples/counter.ex
@@ -2,47 +2,45 @@ defmodule Examples.Counter do
   use ElixirSessions
   @moduledoc false
 
-  def main() do
-    ST.spawn(&server/2, [0], &client/1, [])
-  end
-
-  @session "count = &{?increment().count, ?stop().!value(number).end}"
+  @session "counter = &{?incr(number).counter,
+                      ?stop().!value(number).end}"
   @spec server(pid, number) :: atom
-  def server(client, value) do
+  def server(client, tot) do
     receive do
-      {:increment} ->
-        server(client, value + 1)
-
-        {:stop} ->
-        send(client, {:value, value})
-        :ok
+      {:incr, val} -> server(client, tot + val)
+      {:stop} -> terminate(client, tot)
     end
   end
 
-  @dual "count"
+  @spec terminate(pid, number) :: atom
+  defp terminate(client, tot) do
+    send(client, {:value, tot})
+    :ok
+  end
+
+  @dual "counter"
   @spec client(pid) :: number
   def client(server) do
-    send(server, {:increment})
-    send(server, {:increment})
-    send(server, {:increment})
+    send(server, {:incr, 5})
+    # send(server, {:decr, 2})
     send(server, {:stop})
 
     receive do
-      {:value, num} ->
-        # IO.puts("Counter = " <> num)
-        num
+      {:value, val} ->
+        IO.puts(val)
+        val
     end
   end
 
-  # @dual "count"
-  # @spec incorrect_client(pid) :: number
-  # def incorrect_client(server) do
-  #   send(server, {:increment})
-  #   send(server, {:decrement})
-  #   # send(server, {:stop})
-
-  #   receive do
-  #     {:value, num} -> num
-  #   end
+  # Client with issues
+  # @dual "counter"
+  # @spec misbehaving_client(pid) :: number
+  # def misbehaving_client(server) do
+  #   send(server, {:incr, 5})
+  #   send(server, {:decr, 2})
   # end
+
+  def main do
+    ST.spawn(&server/2, [0], &client/1, [])
+  end
 end
