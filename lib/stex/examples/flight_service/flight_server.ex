8636477b3ce5da0417c240b37c88314aa17a631a
Gerard <gerardtabone@yahoo.com>
2021-08-18 15:04:12 +0200

    flight server

--- lib/stex/examples/flight_service/flight_server.ex
+++ lib/stex/examples/flight_service/flight_server.ex
@@ -57,7 +57,7 @@ defmodule Examples.FlightServer do
 
     origin = "MLA"
     destination = "LUX"
-    departure_date = "2021-09-21"
+    departure_date = "2021-10-21"
     class = :economy
     passengers = 3
 
@@ -75,37 +75,86 @@ defmodule Examples.FlightServer do
       }
     }
 
-    Poison.encode!(body)
-    |> IO.puts()
+    resp =
+      case Duffel.post("offer_requests", Poison.encode!(body), [], timeout: 10000, recv_timeout: 100_000) do
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          # Error
+          {:error, "Error while sending message: #{inspect(reason)}"}
 
-    resp = Duffel.post("offer_requests", Poison.encode!(body))
+        {:ok,
+         %HTTPoison.Response{
+           body: body,
+           status_code: status_code
+         }}
+        when status_code >= 300 ->
+          # Error
+          error = Poison.decode!(body)["errors"]
+          {:error, hd(error)["message"]}
 
-    case resp do
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        # Error
-        {:error, "Error while sending message: #{inspect(reason)}"}
+        {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code >= 200 and status_code < 300 ->
+          # Ok
+          {:ok, Poison.decode!(body)["data"]["offers"]}
+      end
 
-      {:ok,
-       %HTTPoison.Response{
-         body: body,
-         status_code: status_code
-       }}
-      when status_code >= 300 ->
-        # Error
-        error = Poison.decode!(body)["errors"]
-        {:error, hd(error)["message"]}
-
-      {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code >= 200 and status_code < 300 ->
-        # Ok
-        {:ok, Poison.decode!(body)["data"]}
-    end
-
-    # File.write!("flight.json", resp)
-
-    # Poison.decode!(resp)
+      next_offer(resp)
   end
 
-  def establishConnection do
-    :ok
+  defp next_offer({:ok, []}) do
+    throw("No more offers available")
+  end
+
+  defp next_offer({:ok, [next_offer | other_offers]}) do
+    IO.inspect(process_offer(next_offer))
+
+    cont = IO.gets("next y/n:")
+
+    case cont do
+      "n\n" ->
+        :ok
+
+      _ ->
+        next_offer({:ok, other_offers})
+    end
+  end
+
+  defp next_offer({:error, message}) do
+    throw(message)
+  end
+
+  defp process_offer(offer) do
+    id = offer["id"]
+    {total_amount, _} = Float.parse(offer["total_amount"])
+    currency = offer["total_currency"]
+    slices = offer["slices"]
+    segments = hd(slices)["segments"]
+    stops = length(segments) - 1
+    duration = hd(slices)["duration"]
+
+    %{id: id, total_amount: total_amount, currency: currency, duration: duration, stops: stops, segments: process_segments(segments)}
+  end
+
+  defp process_segments(segments) when is_list(segments) do
+    for segment <- segments do
+      origin_city_name = segment["origin"]["city_name"]
+      origin_iata_code = segment["origin"]["iata_code"]
+      destination_city_name = segment["destination"]["city_name"]
+      destination_iata_code = segment["destination"]["iata_code"]
+      departing_at = segment["departing_at"]
+      arriving_at = segment["arriving_at"]
+      marketing_carrier = segment["marketing_carrier"]["name"]
+      marketing_carrier_code = segment["marketing_carrier"]["iata_code"]
+      marketing_carrier_flight_number = segment["marketing_carrier_flight_number"]
+
+      %{
+        origin_city_name: origin_city_name,
+        origin_iata_code: origin_iata_code,
+        destination_city_name: destination_city_name,
+        destination_iata_code: destination_iata_code,
+        marketing_carrier: marketing_carrier,
+        departing_at: departing_at,
+        arriving_at: arriving_at,
+        flight_number: marketing_carrier_code <> marketing_carrier_flight_number
+      }
+    end
   end
 end
